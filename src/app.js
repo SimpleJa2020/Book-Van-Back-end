482e1bc7c358b5e90cef0ef6cd50c1f363656a1c
SimpleJa2020 <ja_37@hotmail.co.th>
2023-02-05 23:54:27 +0700

    config essential middlewares

--- src/app.js
+++ src/app.js
@@ -1,2 +1,36 @@
 // const { sequelize } = require('./models');
 // sequelize.sync({ force: true });
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+
+const notFoundMiddleware = require('./middlewares/not-found');
+const errorMiddleware = require('./middlewares/error');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(
+    rateLimit({
+        windowMs: 1000 * 60,
+        max: 100,
+        message: { message: 'too many request, please try again later' }
+    })
+);
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 2000;
+app.listen(port, () =>
+    console.log(
+        chalk.yellowBright.italic.bold(`server running on port: ${port}`)
+    )
+);
